82bf08f42fc8878892b568fafd0f67575b3dcc84
nbourre <nbourrereg@hotmail.com>
2020-01-14 13:49:23 -0500

    Restructuration et retrait des projets WPF

---
+++ wpf_rappel_polymorphism/IStorable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rappel_polymorphism
+{
+    interface IStorable
+    {
+        void SetData(byte[] data);
+        public byte[] Data { get; set; }
+    }
+}
---
+++ wpf_rappel_polymorphism/MyList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rappel_polymorphism
+{
+    class MyList<T>
+    {
+        List<T> _list;
+
+        public MyList()
+        {
+            _list = new List<T>();
+        }
+    }
+}
